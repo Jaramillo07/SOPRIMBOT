659191bbf176beb4651b9f71535357e24646b5bd
Jaramillo07 <jara_je-90@hotmail.com>
2025-05-19 23:58:19 -0600

    Update __init__.py

--- services/scraper_fanasa/__init__.py
+++ services/scraper_fanasa/__init__.py
@@ -4,7 +4,7 @@ Contiene funcionalidades para extraer información de productos desde FANASA.
 """
 
 # Importar las funciones principales para uso directo
-from .main import buscar_info_medicamento, login_fanasa_carrito, buscar_producto, extraer_info_producto
+from .main import buscar_info_medicamento, login_fanasa_carrito, buscar_producto, extraer_info_productos
 
 # Exposición de funciones principales para importación directa
-__all__ = ['buscar_info_medicamento', 'login_fanasa_carrito', 'buscar_producto', 'extraer_info_producto']
+__all__ = ['buscar_info_medicamento', 'login_fanasa_carrito', 'buscar_producto', 'extraer_info_productos']
